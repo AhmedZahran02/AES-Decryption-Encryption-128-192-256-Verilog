c18faf4f767eefd30b1a5a7161b771d46ee12630
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-05-11 15:44:55 +0200

    AES tested

--- AES.v
+++ AES.v
@@ -2,22 +2,19 @@ module AES(
 input [127:0]in,
 output [127:0]out
 );
-reg [127:0]nkey;
-reg [127:0]bout;
+wire [127:0]key=128'ha0fafe1788542cb123a339392a6c7605;
+wire [127:0]nkey;
+wire [127:0]bout;
     //test input 3243f6a8885a308d313198a2e0370734
     wire [127:0] state [4:0];
     assign state[0] = in;
-//genvar i;
-    //generate
-    //    for(i=0;i <= 3;i=i+1)begin:h
+integer i=0;
+
     subBytes m0(state[0],state[1]);
     Shift_Rows m1(state[2],state[1]);//reversed inputs
-	 inv_Shift_Rows mt(state[2],state[1]);
     MixColumns m2(state[2],state[3]);
-	 //KeyExpansion m3(key,i,nkey);
-	 //AddRoundKey m4(state[3],nkey,bout);
-	//end
-//endgenerate
+//	KeyExpansion m3(key,i,nkey);
+	 AddRoundKey m4(state[3],key,bout);
 
 assign out = bout;
 endmodule
--- KeyExpansion.v
+++ KeyExpansion.v
@@ -3,14 +3,14 @@ input [127:0]key,
 input [3:0] i,
 output [127:0]out
 );
-reg [31:0]sb;
-reg [31:0]rc;
-subByte m0(key[127:96]>>8,sb);
+wire [31:0]sb;
+wire [31:0]rc;
+subBytes m0(key[127:96]>>8,sb);
 Rcon m1(i,rc);
 
 assign out[31:0]=key[31:0]^key[63:32];
 assign out[63:32]=key[63:32]^key[95:64];
-assign out[95:63]=key[95:64]^key[127:96];
+assign out[95:64]=key[95:64]^key[127:96];
 assign out[127:96]=key[127:96]^sb^rc;
  
 endmodule
