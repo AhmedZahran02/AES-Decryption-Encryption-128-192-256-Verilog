9cfbc296271cc16a630692dffb5cc16616c86f8e
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-05-12 22:23:44 +0200

    aes2

--- AES2.v
+++ AES2.v
@@ -3,9 +3,10 @@ module AES2(output [127:0] dataout);
 	wire [127:0] datain;
 	wire [127:0] key;
 	reg [127:0] state;
-	wire [127:0] r_out[0:9];
+	wire [127:0] r_out[0:10];
 	
 	wire [127:0] keyout[1:10];
+	wire [127:0] sb,sr;
 	
 	KeyExpansion m0(key,0001,keyout[1]);
 	genvar i;
@@ -15,8 +16,21 @@ module AES2(output [127:0] dataout);
 	KeyExpansion m1(keyout[i-1],i,keyout[i]);
 	end
 	endgenerate
-	state=datain;
-	state=datain^keyout[10];
+	assign r_out[0]=datain;
+	
+	main2 m2(.in(r_out[0]),.key(key),.out(r_out[1]));
+	genvar ik;
+	generate
+	for(ik=10;ik>=2;ik=ik-1)
+	begin : iter_i2
+	main2 m3(.in(r_out[11-ik]),.key(keyout[ik]),.out(r_out[12-ik]));
+	end
+	endgenerate
+	inv_Shift_Rows t3(sr, r_out[10]);
+	invsubBytes t4(sr,sb);
+	assign dataout=keyout[1]^sb;
+	//state=datain;
+	//state=datain^keyout[10];
 	
 	  
 endmodule
