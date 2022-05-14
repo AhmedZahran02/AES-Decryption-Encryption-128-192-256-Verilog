d45d77764e124804de9a1060ebca4f15c24b4bbd
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-05-14 21:10:00 +0200

    Revert "Merge branch 'main' of https://github.com/AhmedZahran02/Logic-Project"
    This reverts commit 007b785a35b7e7d457f4439f4e5830f3ed2c98be, reversing
    changes made to a499c496046018e5bb5f48e6fed2a859c181f677.

--- main2.v
+++ main2.v
@@ -1,8 +1,7 @@
 module main2(
 input [127:0]in,
 input [127:0]key,
-output [127:0]out,
-output [127:0]keyout
+output [127:0]out
 );
 
 wire [127:0] sb,sr,mcl,ark;
@@ -10,7 +9,7 @@ wire [127:0] sb,sr,mcl,ark;
 	
 inv_Shift_Rows m1(sr,in);//reversed inputs
 invsubBytes m2(sr,sb);
-assign ark=keyout^sb;
+assign ark=key^sb;
 inv_MixColumns m3(ark,mcl);
 
 assign out = mcl;
